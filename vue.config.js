110224a75ec09ab31e8e7ac5cf4d7f1381289657
tkmn7486 <71046924+tkmn7486@users.noreply.github.com>
2023-03-24 09:55:25 +0900

    add manifest

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: 'Todo And Reward',
+    themeColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // Service Worker は必須
+      swSrc: 'dev/sw.js',
+    }
+  }
 })
