47a8d88415c33704cac72ee20ec484b6cf226b22
tkmn7486 <71046924+tkmn7486@users.noreply.github.com>
2023-03-24 10:12:03 +0900

    add icons

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  pwa: {
-    name: 'Todo And Reward',
-    themeColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // Service Worker は必須
-      swSrc: 'dev/sw.js',
-    }
-  }
 })
